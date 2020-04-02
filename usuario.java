b3a40c26b55e3201027975f28c8177d9fdcf8459
nicolas <nicolas@coinafrique.com>
2020-04-02 19:53:20 -0300

    B - Implementación de metodos: getters, setters y constructor.

--- usuario.java
+++ usuario.java
@@ -6,4 +6,51 @@ public class Usuario {
     int cantidadDeHijos;
     int saldo = 0;
 
+    public void setNombre(String nombre) {
+        nombre = nombre
+    }
+    public void setCedula(int cedula) {
+        cedula = cedula
+    }
+    public void setContrasena(String contrasena) {
+        contrasena = contrasena
+    }
+    public void setEdad(int edad) {
+        edad = edad
+    }
+    public void setCantidadDeHijos(int cantidadDeHijos) {
+        cantidadDeHijos = cantidadDeHijos
+    }
+    public void setSaldo(int saldo) {
+        saldo = saldo
+    }
+
+    public String getNombre() {
+        return nombre
+    }
+    public int getCedula() {
+        return cedula
+    }
+    public String getContrasena() {
+        return contrasena
+    }
+    public int getEdad() {
+        return edad
+    }
+    public int getCantidadDeHijos() {
+        return cantidadDeHijos
+    }
+    public int getSaldo() {
+        return saldo
+    }
+
+    Usuario (String nom, int ci, String password, int edadUsuario, int hijos, int sal) {
+        nombre = nom;
+        cedula = ci;
+        contrasena = password;
+        edad = edadUsuario;
+        cantidadDeHijos = hijos;
+        saldo = sal;
+    }
+
 }
